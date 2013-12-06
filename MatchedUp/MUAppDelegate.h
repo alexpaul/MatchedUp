eae4e402c2c06b01c7683f0d281296006fa3f6ca
Alex Paul <axp_alpaul@yahoo.com>
2013-12-06 12:19:15 -0500

    Initial Commit

---
+++ MatchedUp/MUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MUAppDelegate.h
+//  MatchedUp
+//
+//  Created by Alex Paul on 12/6/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MatchedUp/MUSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  MUSecondViewController.h
+//  MatchedUp
+//
+//  Created by Alex Paul on 12/6/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUSecondViewController : UIViewController
+
+@end
